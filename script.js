aced7ac2e0ea7e0c7d436699471892a193eacbc2
Jane McPheron <jvmcpheron@Janes-MacBook-Pro.local>
2022-07-09 15:21:36 -0500

    working suggested section, however still an issue with local storage resetting

--- script.js
+++ script.js
@@ -44,32 +44,30 @@ searchBtn.addEventListener("click" , ()=>{
     city = document.getElementById("citySearch").value;
     if (city === localStorage.getItem('suggestedOne')){
         console.log("already top result");
-    } if (city != localStorage.getItem('suggestedOne')){
-        if (city === localStorage.getItem('suggestedTwo')){
-            suggestedTwo = localStorage.getItem('suggestedOne');
+    }else if (localStorage.getItem('suggestedOne') === "No City Yet"){
             suggestedOne = city;
-        } if (city === localStorage.getItem('suggestedThree')) {
-            suggestedThree = localStorage.getItem('suggestedTwo');
-            suggestedTwo = localStorage.getItem('suggestedOne');
+    }else if (localStorage.getItem('suggestedTwo') === "No City Yet"){
+        suggestedTwo = suggestedOne;
+        suggestedOne = city;
+    }else {
+        if (city === localStorage.getItem('suggestedTwo')){
+            suggestedTwo = suggestedOne;
             suggestedOne = city;
         } else {
-            if (suggestedTwo != "No City Yet" && suggestedThree != "No City Yet") {
-            suggestedThree = localStorage.getItem('suggestedTwo');
-            suggestedTwo = localStorage.getItem('suggestedOne');
+            suggestedThree = suggestedTwo;
+            suggestedTwo = suggestedOne;
             suggestedOne = city;
-            } if (suggestedTwo === "No City Yet"){
-                suggestedTwo = localStorage.getItem('suggestedOne');
-                suggestedOne = city;
-            } else {
-                suggestedThree = localStorage.getItem('suggestedTwo');
-                suggestedTwo = localStorage.getItem('suggestedOne');
-                suggestedOne = city;
-            }
-        } 
+        }
+
     }
+        
       localStorage.setItem('suggestedOne' , suggestedOne);
       localStorage.setItem('suggestedTwo' , suggestedTwo);
       localStorage.setItem('suggestedThree' , suggestedThree);
+
+      document.getElementById('city-one').textContent = localStorage.getItem('suggestedOne');
+document.getElementById('city-two').textContent = localStorage.getItem('suggestedTwo');
+document.getElementById('city-three').textContent = localStorage.getItem('suggestedThree');
 
     document.getElementById("city").innerHTML = city
     console.log(city);
@@ -102,3 +100,109 @@ searchBtn.addEventListener("click" , ()=>{
   
 });
 
+
+
+//suggested search call
+
+document.getElementById('city-one').addEventListener("click" , ()=>{
+    city = localStorage.getItem('suggestedOne');
+    
+
+    document.getElementById("city").innerHTML = city
+    console.log(city);
+    getData = fetch(`http://api.weatherapi.com/v1/forecast.json?key=59d51aff817d40719e5231234220807&q=${city}&days=7`)
+  .then(function(response) {
+    if (response.ok) {
+        return response.json();
+    }
+  })
+  .then (function(data) {
+    console.log(data);
+    document.getElementById("city").innerHTML = city
+    document.getElementById("main-temp").innerHTML = data.current.temp_f;
+    document.getElementById("main-wind").innerHTML = data.current.gust_mph;
+    document.getElementById("main-hum").innerHTML = data.current.humidity;
+    document.getElementById("temp-two").innerHTML = data.forecast.forecastday[1].day.avgtemp_f;
+    document.getElementById("wind-two").innerHTML = data.forecast.forecastday[1].day.maxwind_mph;
+    document.getElementById("hum-two").innerHTML = data.forecast.forecastday[1].day.avghumidity;
+    document.getElementById("temp-three").innerHTML = data.forecast.forecastday[2].day.avgtemp_f;
+    document.getElementById("wind-three").innerHTML = data.forecast.forecastday[2].day.maxwind_mph;
+    document.getElementById("hum-three").innerHTML = data.forecast.forecastday[2].day.avghumidity;
+
+    
+
+
+  });
+
+
+
+  
+})
+document.getElementById('city-two').addEventListener("click" , ()=>{
+    city = localStorage.getItem('suggestedTwo');
+    
+
+    document.getElementById("city").innerHTML = city
+    console.log(city);
+    getData = fetch(`http://api.weatherapi.com/v1/forecast.json?key=59d51aff817d40719e5231234220807&q=${city}&days=7`)
+  .then(function(response) {
+    if (response.ok) {
+        return response.json();
+    }
+  })
+  .then (function(data) {
+    console.log(data);
+    document.getElementById("city").innerHTML = city
+    document.getElementById("main-temp").innerHTML = data.current.temp_f;
+    document.getElementById("main-wind").innerHTML = data.current.gust_mph;
+    document.getElementById("main-hum").innerHTML = data.current.humidity;
+    document.getElementById("temp-two").innerHTML = data.forecast.forecastday[1].day.avgtemp_f;
+    document.getElementById("wind-two").innerHTML = data.forecast.forecastday[1].day.maxwind_mph;
+    document.getElementById("hum-two").innerHTML = data.forecast.forecastday[1].day.avghumidity;
+    document.getElementById("temp-three").innerHTML = data.forecast.forecastday[2].day.avgtemp_f;
+    document.getElementById("wind-three").innerHTML = data.forecast.forecastday[2].day.maxwind_mph;
+    document.getElementById("hum-three").innerHTML = data.forecast.forecastday[2].day.avghumidity;
+
+    
+
+
+  });
+
+
+
+  
+})
+document.getElementById('city-three').addEventListener("click" , ()=>{
+    city = localStorage.getItem('suggestedThree');
+    
+
+    document.getElementById("city").innerHTML = city
+    console.log(city);
+    getData = fetch(`http://api.weatherapi.com/v1/forecast.json?key=59d51aff817d40719e5231234220807&q=${city}&days=7`)
+  .then(function(response) {
+    if (response.ok) {
+        return response.json();
+    }
+  })
+  .then (function(data) {
+    console.log(data);
+    document.getElementById("city").innerHTML = city
+    document.getElementById("main-temp").innerHTML = data.current.temp_f;
+    document.getElementById("main-wind").innerHTML = data.current.gust_mph;
+    document.getElementById("main-hum").innerHTML = data.current.humidity;
+    document.getElementById("temp-two").innerHTML = data.forecast.forecastday[1].day.avgtemp_f;
+    document.getElementById("wind-two").innerHTML = data.forecast.forecastday[1].day.maxwind_mph;
+    document.getElementById("hum-two").innerHTML = data.forecast.forecastday[1].day.avghumidity;
+    document.getElementById("temp-three").innerHTML = data.forecast.forecastday[2].day.avgtemp_f;
+    document.getElementById("wind-three").innerHTML = data.forecast.forecastday[2].day.maxwind_mph;
+    document.getElementById("hum-three").innerHTML = data.forecast.forecastday[2].day.avghumidity;
+
+    
+
+
+  });
+
+
+
+  
+})
